c9505bca057e0f800c787d6d234f446abf2f2ba5
Abdul Razzak <abdulrazzak74832@gmail.com>
2024-04-25 00:25:31 +0530

    node mailer options and setup started

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,25 @@
+import nodemailer from 'nodemailer';
+
+export const sendEmail = async({email , emailType, userId}:any) =>{
+    try {
+        const transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false, // Use `true` for port 465, `false` for all other ports
+            auth: {
+              user: "maddison53@ethereal.email",
+              pass: "jn7jnAPss4f63QBp6D",
+            },
+          });
+          const mailOptions = {
+            from: 'abdul123@gmail.com', // sender address
+            to: "bar@example.com, baz@example.com", // list of receivers
+            subject: "Hello ✔", // Subject line
+            text: "Hello world?", // plain text body
+            html: "<b>Hello world?</b>", // html body
+          }
+          const info = await transporter.sendMail(mailOptions);
+    } catch (error) {
+        
+    }
+}
