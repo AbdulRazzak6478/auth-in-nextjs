1507e0ac7ec0a7957d536979624d233397bd8d6c
Abdul Razzak <abdulrazzak74832@gmail.com>
2024-04-28 02:35:35 +0530

    auth profile ui page added

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
-
+'use client'
+import React, { useState } from 'react'
+import "../../styles/signup.css";
 const page = () => {
+    const [load, setLoad] = useState(false);
+    const getUserDetails = () =>{
+        setLoad(true);
+    }
+    const onLogout = ()=>{
+        setLoad(true);
+
+    }
   return (
-    <div>page</div>
+    <div>
+        <div className="verify-box">
+            <div className="data">
+                <h2>User Id : </h2>
+                <p>User Name : </p>
+                <p>Email : </p>
+                <button className='submit' onClick={onLogout}>{load ? 'Loading...':'Logout'}</button>
+            </div>
+            <button className='submit' onClick={getUserDetails}>{load ? 'Getting User Details ...':'Get User Details'}</button>
+        </div>
+    </div>
   )
 }
 
